f62a44ef9130a7928877e3d78f61bc6840add3bd
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-09-18 00:36:03 -0400

    Use custom fonts for the html body by updating internal layout style sheet

--- server/lib/in-memory-db.js
+++ server/lib/in-memory-db.js
@@ -2,6 +2,7 @@
 
 // Requiring a JSON file automatically parses it and returns the data. These
 // are just example tweets to make it less tedious to style the app initially.
+// db is an object containing an array of multiple nested objects.
 const db = {
   tweets: require("../data-files/initial-tweets")
 }
